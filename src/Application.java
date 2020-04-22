b44b919d3692ea0073541e0d80cd5662a741cef5
nathanwiley18 <59708070+nathanwiley18@users.noreply.github.com>
2020-04-21 20:46:54 -0400

    Add files via upload

--- src/Application.java
+++ src/Application.java
@@ -1,7 +1,10 @@
-public class Application {
-public static void main(String[] args){
-	DuplicateRemover duplicateRemover=new DuplicateRemover();
-	duplicateRemover.remove("problem1.txt");
-	duplicateRemover.write("unique_words.txt");
-}
+public class Application{
+   public static void main(String[] args){
+       String inFilename = "problem2.txt";
+       String outFilename = "unique_word_counts.txt";
+       DuplicateCounter duplicateCounter = new DuplicateCounter();
+       duplicateCounter.count(inFilename);
+       duplicateCounter.write(outFilename);
+       System.out.println("Please check the output file: " + outFilename);
+   }
 }
